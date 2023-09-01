65a7cea434a061343966707d398502372089b3ab
LucasLopesLedur <lucasledur@icloud.com>
2023-08-31 22:42:42 -0300

    f

---
+++ src/main/java/med/voll/api/medico/DadosAtualizaMedico.java
@@ -0,0 +1,12 @@
+package med.voll.api.medico;
+
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAtualizaMedico(
+        @NotNull
+        Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco) {
+
+}
